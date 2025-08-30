f4dddb75d97e1778e416afaab1b4b9f7e76c99e4
Roland Meier <43388268+rm369@users.noreply.github.com>
2025-08-30 15:29:09 +0200

    - renew access token when necessary - wait in case of too many requests - get spotify_credentials from yaml file - get root path as (only) command line argument - update album title and cover only if changed

--- album_info_art_fetcher.py
+++ album_info_art_fetcher.py
@@ -1,40 +1,67 @@
+import argparse
 import os
+import sys
+import time
 import requests
 import mutagen
+import yaml
 from mutagen.id3 import ID3, TALB, TPE1, TIT2, APIC, ID3NoHeaderError
 from requests.auth import HTTPBasicAuth
 
-# Spotify API credentials to change by user
-SPOTIFY_CLIENT_ID = 'SPOTIFY_CLIENT_ID'
-SPOTIFY_CLIENT_SECRET = 'SPOTIFY_CLIENT_SECRET'
+# Spotify API credentials
+spotify_credentials_file = "spotify_credentials.yaml"
+spotify_credentials = {}
+access_token = None
 
-def get_spotify_access_token(client_id, client_secret):
+def get_spotify_access_token():
+    global access_token
     url = "https://accounts.spotify.com/api/token"
     headers = {
-        "Authorization": "Basic " + (client_id + ":" + client_secret).encode("ascii").decode("ascii"),
+        "Authorization": "Basic " + (spotify_credentials['id'] + ":" + spotify_credentials['secret']).encode("ascii").decode("ascii"),
     }
     data = {
         "grant_type": "client_credentials",
     }
-    response = requests.post(url, headers=headers, data=data, auth=HTTPBasicAuth(client_id, client_secret))
+    response = requests.post(url, headers=headers, data=data, auth=HTTPBasicAuth(spotify_credentials['id'], spotify_credentials['secret']))
     response_data = response.json()
     if "access_token" in response_data:
-        return response_data["access_token"]
+        access_token = response_data["access_token"]
     else:
-        print(f"Error obtaining access token: {response_data}")
-        return None
+        raise Exception(f"Error obtaining access token: {response_data}")
+    return None
 
-def get_album_info_from_spotify(access_token, artist, title):
-    search_url = "https://api.spotify.com/v1/search"
-    headers = {
-        "Authorization": f"Bearer {access_token}",
-    }
+
+def get_from_spotify(url, params):
+    global access_token
+    retry = True
+    while retry:
+        headers = {
+            "Authorization": f"Bearer {access_token}",
+        }
+        response = requests.get(url, headers=headers, params=params)
+        if response.status_code == 429: # 'Too many requests'
+            retry_after_time = response.headers['retry-after']
+            print(f"Too many requests, sleeping {retry_after_time} seconds...")
+            for i in range(int(retry_after_time), 0, -1):
+                sys.stdout.write("\r")
+                sys.stdout.write(str(i) + ' ')
+                sys.stdout.flush()
+                time.sleep(1)
+                response = requests.get(url, headers=headers, params=params) # retry
+        elif response.status_code == 401: # 'The access token expired'
+            get_spotify_access_token()
+        else:
+            # retry = False
+            return response
+
+def get_album_info_from_spotify(artist, title):
+    url = "https://api.spotify.com/v1/search"
     params = {
         "q": f"artist:{artist} track:{title}",
         "type": "track",
         "limit": 1,
     }
-    response = requests.get(search_url, headers=headers, params=params)
+    response = get_from_spotify(url, params)
     response_data = response.json()
 
     # Debugging: Print the response data
@@ -50,14 +77,11 @@ def get_album_info_from_spotify(access_token, artist, title):
         print(f"No suitable album found for artist: {artist} and title: {title}. Album name will not be changed.")
         return None, None
 
-def get_cover_art_from_spotify(access_token, album_id):
+def get_cover_art_from_spotify(album_id):
     if not album_id:
         return None
     album_url = f"https://api.spotify.com/v1/albums/{album_id}"
-    headers = {
-        "Authorization": f"Bearer {access_token}",
-    }
-    response = requests.get(album_url, headers=headers)
+    response = get_from_spotify(album_url, {})
     response_data = response.json()
     if response_data.get('images'):
         cover_art_url = response_data['images'][0]['url']
@@ -68,7 +92,7 @@ def get_cover_art_from_spotify(access_token, album_id):
         print(f"No cover art found for album ID: {album_id}")
         return None
 
-def update_file_metadata(file_path, access_token):
+def update_file_metadata(file_path):
     print(f"Processing file: {file_path}")
     try:
         audio = ID3(file_path)
@@ -91,38 +115,47 @@ def update_file_metadata(file_path, access_token):
     
     print(f"Artist: {artist}, Title: {title}")
     
-    album_title, album_id = get_album_info_from_spotify(access_token, artist, title)
-    
-    if album_title:
+    album_title, album_id = get_album_info_from_spotify(artist, title)
+
+    updated = False
+    if album_title and album_title != audio.get('TALB', None):
         audio.delall('TALB')
         audio.add(TALB(encoding=3, text=album_title))
-    
-    cover_art_data = get_cover_art_from_spotify(access_token, album_id)
-    if cover_art_data:
-        audio.delall('APIC')
-        audio.add(APIC(
-            encoding=3,
-            mime='image/jpeg',
-            type=3, desc='Cover',
-            data=cover_art_data
-        ))
-    
-    audio.save(file_path)
-    print(f"Updated file: {file_path} with album: {album_title if album_title else 'No change'}")
+        updated = True
 
-def process_folder(folder_path, access_token):
+    cover_art_data_old = audio.get('APIC:Cover', None)
+    if album_id: # and not cover_art_data_old:
+        cover_art_data = get_cover_art_from_spotify(album_id)
+        if cover_art_data != cover_art_data_old:
+            audio.delall('APIC')
+            audio.add(APIC(
+                encoding=3,
+                mime='image/jpeg',
+                type=3, desc='Cover',
+                data=cover_art_data
+            ))
+            updated = True
+
+    if updated:
+        audio.save(file_path)
+        print(f"Updated file: {file_path} with album: {album_title if album_title else 'No change'}")
+
+def process_folder(folder_path):
     for root, _, files in os.walk(folder_path):
         for file in files:
             if file.endswith(('.mp3', '.flac', '.ogg', '.m4a')):
                 file_path = os.path.join(root, file)
-                update_file_metadata(file_path, access_token)
+                update_file_metadata(file_path)
 
 if __name__ == "__main__":
-    #To be changed by user
-    folder_path = "/path/to/your/folder/with/music"
-    access_token = get_spotify_access_token(SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET)
-    if access_token:
-        process_folder(folder_path, access_token)
-    else:
-        print("Failed to obtain access token. Exiting.")
 
+    with open(spotify_credentials_file, "r") as f:
+        spotify_credentials = yaml.safe_load(f)
+    get_spotify_access_token()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path")
+    args = parser.parse_args()
+    folder_path = args.path
+
+    process_folder(folder_path)
